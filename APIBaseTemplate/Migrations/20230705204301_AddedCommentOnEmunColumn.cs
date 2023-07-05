af60423c3ea2664a3c8286e00d23fd9e61546afa
Simone CAPPELLETTI <simonecappelletti@tiscali.it>
2023-07-06 00:18:14 +0200

    Added migration folder

---
+++ APIBaseTemplate/Migrations/20230705204301_AddedCommentOnEmunColumn.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIBaseTemplate.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCommentOnEmunColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PriceType",
+                table: "FligthServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                comment: "FlightServiceType values (Fligth = 0, HandLuggage = 1, HoldLuggage = 2, FligthInsurance = 3)",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PriceType",
+                table: "FligthServices",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldComment: "FlightServiceType values (Fligth = 0, HandLuggage = 1, HoldLuggage = 2, FligthInsurance = 3)");
+        }
+    }
+}
